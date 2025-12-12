ccd159e48b311b45a235afc04873dbbc75cd3a60
Chirag <113755144+chirag-dua123@users.noreply.github.com>
2025-12-12 12:19:16 +0530

    new change

--- tests/test_core.py
+++ tests/test_core.py
@@ -7,5 +7,10 @@ def test_multiply():
 def test_run_calc():
     assert calculator.run_calculator(4, 2, '/') == 2
 
-with pytest.raises(ValueError):
-    calculator.run_calculator(1, 2, '%')
+def test_invalid_operator():
+    with pytest.raises(ValueError):
+        calculator.run_calculator(1, 2, '%')
+
+def test_divide_by_zero():
+    with pytest.raises(ZeroDivisionError):
+        calculator.run_calculator(4, 0, '/')
